06030f315d84d43bdc467dae736882bea88f1808
savallarian45@gmail.com <savallarian45@gmail.com>
2025-04-27 22:05:41 -0300

    primer commit

---
+++ src/main/java/datos/Cliente.java
@@ -0,0 +1,11 @@
+package datos;
+
+import java.util.List;
+
+public class Cliente {
+	private long idCliente;
+	private List<Ticket> ticketsHistorial;
+	private List<Ticket> ticketsAbiertos;
+	private String area;
+
+}
---
+++ src/main/java/datos/Usuario.java
@@ -0,0 +1,11 @@
+package datos;
+
+public abstract class Usuario {
+	
+	private String nombre;
+	private String apellido;
+	private String email;
+	private String password;
+	private String cuil;
+	
+}
